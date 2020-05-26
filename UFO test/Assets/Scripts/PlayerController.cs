f4f5399a6080a97ae464c1e0d3bc0ebd262e1d2b
nicolas-laiton <54083510+nicolas-laiton@users.noreply.github.com>
2020-05-26 09:11:46 -0500

    SetCountTextTest

--- UFO test/Assets/Scripts/PlayerController.cs
+++ UFO test/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,6 @@ public class PlayerController : MonoBehaviour {
 	public float speed;				//Floating point variable to store the player's movement speed.
 	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
 	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
-
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
 	private int count;				//Integer to store the number of pickups collected so far.
 
---
+++ UFO test/Assets/Tests/TestSuites.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+    public class PlayerControllerTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SetCountTextTest()
+        {/*
+            public float speed;				//Floating point variable to store the player's movement speed.
+            public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
+            public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
+            private int count=8;				//Integer to store the number of pickups collected so far.
+            
+            public countText.text = "";			
+	        public winText.text = "";
+            //var out= playerController.SetCountText();
+            Assert.AreEqual("Count: 8",countText.text);
+            Assert.AreEqual("You win!",winText.text);*/
+        }
+        /*
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }*/
+    }
+
